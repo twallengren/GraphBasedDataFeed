bf0baa5c6344e64d4ad67b3c7ae0704a7b1e1755
toren.wallengren <toren.wallengren@gmail.com>
2022-05-30 18:39:53 +0100

    organize main

--- src/main/java/org/example/Main.java
+++ src/main/java/org/example/Main.java
@@ -7,7 +7,14 @@ import java.util.function.Function;
 
 public class Main {
     public static void main(String[] args) {
+        int numOfNodes = 500;
+        DataFeedNetwork<Integer,String> network = getCollatzNetwork("0", 100);
+        DataFeedDataPacket<Integer,String> dataFeedDataPacket = new DataFeedDataPacket<>(27, "27");
+        DataFeedDataPacket<Integer,String> test = network.evaluatePath("0", "" + (numOfNodes - 1), dataFeedDataPacket);
+        System.out.println("Final value is: " + test);
+    }
 
+    private static DataFeedNetwork<Integer,String> getCollatzNetwork(String networkId, int numOfNodes) {
         Function<Integer,Integer> preProcessingRule = X -> {
             if (X % 2 == 0) {
                 return X/2;
@@ -19,22 +26,28 @@ public class Main {
             return Y + " " + Z;
         };
         BiFunction<Integer,String,Boolean> triggerRule = (X,Y) -> X != 1;
+        return getHomogeneousNetworkFromFunctions(networkId, numOfNodes, preProcessingRule, dataTransferRule, aggregatingRule, triggerRule);
+    }
 
-        int numOfNodes = 500;
+    private static <X,Y,Z> DataFeedNetwork<X,Y> getHomogeneousNetworkFromFunctions(
+            String networkId,
+            int numOfNodes,
+            Function<X,X> preProcessingRule,
+            Function<X,Z> dataTransferRule,
+            BiFunction<Z,Y,Y> aggregatingRule,
+            BiFunction<X,Y,Boolean> triggerRule) {
+
         List<String> nodeIds = new ArrayList<>();
         for (int n = 0; n < numOfNodes; n++) {
             nodeIds.add(String.valueOf(n));
         }
-        DataFeedNetwork.Builder<Integer,String> networkBuilder = new DataFeedNetwork.Builder<>("0");
+        DataFeedNetwork.Builder<X,Y> networkBuilder = new DataFeedNetwork.Builder<>(networkId);
         for (int n = 0; n < numOfNodes; n++) {
             networkBuilder.addNode(nodeIds.get(n), preProcessingRule, dataTransferRule, aggregatingRule, triggerRule);
         }
         for (int n = 0; n < numOfNodes-1; n++) {
             networkBuilder.addConnection(String.valueOf(n), String.valueOf(n+1));
         }
-        DataFeedNetwork<Integer,String> network = networkBuilder.build();
-        DataFeedDataPacket<Integer,String> dataFeedDataPacket = new DataFeedDataPacket<>(27, "27");
-        DataFeedDataPacket<Integer,String> test = network.evaluatePath(nodeIds.get(0), nodeIds.get(numOfNodes-1), dataFeedDataPacket);
-        System.out.println("Final value is: " + test);
+        return networkBuilder.build();
     }
 }
